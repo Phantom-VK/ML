db192a3a1e81910ace3a4546a5a840794dbfc213
VikramadityaKhupse <2022bit052@sggs.ac.in>
2024-11-15 11:55:13 +0530

    Initial commit and numpy practice

---
+++ numpy_practice.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr1 = np.array(((1, 2, 3, 4, 5, 5), (1, 2, 3, 3, 4, 5 )))
+arr1 = arr1.reshape(6, 2)
+print(arr1)
+print(arr1.shape)
+
+arr2 = np.arange(0,10)
+print(arr2)
+arr3 = np.ones((1, 10))
+print(arr3)
